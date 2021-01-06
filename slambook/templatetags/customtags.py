790ea63c9d70b6666f9b55f93fbc49228d5fe6d9
ribson068 <ribsonmotoe@gmail.com>
2021-01-06 22:36:05 +0530

    notification format_mess working

--- slambook/templatetags/customtags.py
+++ slambook/templatetags/customtags.py
@@ -1,9 +1,9 @@
 from django import template
 from slambook.models import Contributor
-from slambook.models import Notifications
+from slambook.models import Notifications,Type_Of_Notification
 
 register = template.Library()
-
+mess=""
 @register.filter
 def responded_gifts(value):
     return Contributor.objects.filter(giftchart=value,response=True).count()
@@ -25,6 +25,7 @@ def format_message(var, args):
         return n.typeofNotification.notif_main_message 
 #    arg_list = [arg.strip() for arg in args.split(',')]
     #n.typeofNotification.notif_main_message.format()
+    print("Hello")
     print(args)
     return "working"
 
@@ -32,3 +33,17 @@ def format_message(var, args):
 @register.filter
 def concat_string(value_1, value_2):
     return str(value_1) + str(value_2)
+
+
+
+@register.filter
+def format_mess(var):
+    n=Notifications.objects.get(pk=var)
+    tn=Type_Of_Notification.objects.get(pk=n.typeofNotification.pk)   
+    arg=n.arguments    
+    arg=arg.split(',')
+    m=tn.notif_main_message
+    foutput=m.format(*arg)
+    #n.typeofNotification.notif_main_message.format()
+    return foutput
+
